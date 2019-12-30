302c98445b30bce5020463e3c63024bfc77711c7
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-12-30 14:13:49 +0900

    Modalを表示させるボタンを追加

---
+++ UIPresentationController/UIPresentationController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPresentationController
+//
+//  Created by home on 2019/12/30.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
