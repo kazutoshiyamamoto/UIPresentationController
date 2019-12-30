2b2171ad1ced70400fe4d9cf8006d145188459e4
kazutoshi.yamamoto <ghurlhfjkrd@gmail.com>
2019-12-30 14:19:26 +0900

    UIPresentationControllerを継承したクラスを追加

---
+++ UIPresentationController/UIPresentationController/CustomPresentationController.swift
@@ -0,0 +1,13 @@
+//
+//  CustomPresentationController.swift
+//  UIPresentationController
+//
+//  Created by home on 2019/12/30.
+//  Copyright © 2019 Swift-beginners. All rights reserved.
+//
+
+import UIKit
+
+class CustomPresentationController: UIPresentationController {
+
+}
